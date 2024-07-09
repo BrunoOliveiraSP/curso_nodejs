0d5ef66ab8626b4cb81cd8ec0e29e02713d9c288
Bruno O. <brunodeoliveira.22.10@gmail.com>
2024-07-09 10:15:30 -0300

    primeiro endpoint

--- app.js
+++ app.js
@@ -1,4 +1,15 @@
+import express from 'express';
 
-console.log('Oie, em breve serei uma API! =) ');
+const servidor = express();
 
-// 
+
+servidor.get('/helloworld', (req, resp) => {
+    // código do endpoint
+    resp.send('Hello world!!! =))');
+})
+
+
+
+servidor.listen(
+    5001,
+    () => console.log('---> API subiu com sucesso na porta 5001!'));
